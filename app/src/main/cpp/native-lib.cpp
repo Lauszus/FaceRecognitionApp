613d46a8bc0bb1d6afbbfd81af997a03e1cb44aa
Kristian Sloth Lauszus <lauszus@gmail.com>
2016-10-27 21:31:23 -0500

    Live camera feed is now working

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,108 @@
 #include <jni.h>
-#include <string>
+#include <opencv2/core/core.hpp>
 
-extern "C"
-jstring
-Java_com_lauszus_facerecognitionapp_FaceRecognitionAppActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+using namespace std;
+using namespace cv;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline void convertYUVToRGBA(uint8_t y, uint8_t u, uint8_t v, uint8_t *buf) __attribute__((always_inline));
+
+static void convertYUVImageToRGBA(const Mat *pYUV, Mat *pRGB) {
+    const Size size = pRGB->size();
+    const int width = size.width;
+    const int height = size.height;
+    const int n_pixels = width * height;
+    const int rgba_channels = pRGB->channels();
+
+    // See: https://android.googlesource.com/platform/frameworks/av/+/master/media/libstagefright/yuv/YUVImage.cpp,
+    // https://wiki.videolan.org/YUV/#Semi-planar
+    // and https://en.wikipedia.org/wiki/YUV#Y.E2.80.B2UV420p_.28and_Y.E2.80.B2V12_or_YV12.29_to_RGB888_conversion
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            // U and V channels are interleaved as VUVUVU.
+            // So V data starts at the end of Y channel and
+            // U data starts right after V's start.
+            const int yIndex = x + y * width;
+            const uint8_t y_val = pYUV->data[yIndex];
+
+            // Since U and V channels are interleaved, offsets need to be doubled.
+            const int uvOffset = (y >> 1) * (width >> 1) + (x >> 1);
+            const int vIndex = n_pixels + 2*uvOffset;
+            const int uIndex = vIndex + 1;
+            const uint8_t v_val = pYUV->data[vIndex];
+            const uint8_t u_val = pYUV->data[uIndex];
+            convertYUVToRGBA(y_val, u_val, v_val, &pRGB->data[rgba_channels * yIndex]);
+        }
+    }
 }
+
+void Java_com_lauszus_facerecognitionapp_FaceRecognitionAppActivity_YUV2RGB(JNIEnv *, jobject, jlong addrYuv, jlong addrRgba) {
+    Mat *pYUV = (Mat *) addrYuv; // YUV 4:2:0 planar image, with 8 bit Y samples, followed by interleaved V/U plane with 8bit 2x2 sub-sampled chroma samples
+    Mat *pRGB = (Mat *) addrRgba; // RGBA image
+
+    convertYUVImageToRGBA(pYUV, pRGB);
+}
+
+void Java_com_lauszus_facerecognitionapp_FaceRecognitionAppActivity_HistEQ(JNIEnv *env, jobject thiz, jlong addrYuv, jlong addrRgba) {
+    Mat *pYUV = (Mat *) addrYuv; // YUV 4:2:0 planar image, with 8 bit Y samples, followed by interleaved V/U plane with 8bit 2x2 sub-sampled chroma samples
+    Mat *pRGB = (Mat *) addrRgba; // RGBA image
+
+    const Size size = pRGB->size();
+    const int width = size.width;
+    const int height = size.height;
+
+    // 1. Step: Compute histogram of Y channel
+    uint32_t histogram[256];
+    memset(histogram, 0, sizeof(histogram));
+
+    for (int y = 0; y < height; y++) {
+        for (int x = width / 2; x < width; x++) { // Only look at half the image
+            const int yIndex = x + y * width;
+            const uint8_t y_val = pYUV->data[yIndex];
+            histogram[y_val]++;
+        }
+    }
+
+    // Step 2: Compute CDF of histogram
+    uint32_t histogram_cdf[256];
+    memset(histogram_cdf, 0, sizeof(histogram_cdf));
+
+    histogram_cdf[0] = histogram[0];
+    for (int i = 1; i < 256; i++)
+        histogram_cdf[i] = histogram_cdf[i - 1] + histogram[i]; // Calculate CDF
+
+    for (int i = 0; i < 256; i++)
+        histogram_cdf[i] /= histogram_cdf[255] / 255; // Normalize CDF
+
+    // Step 3: Apply histogram equalization
+    for (int y = 0; y < height; y++) {
+        for (int x = width / 2; x < width; x++) { // Image is flipped after this function
+            const int yIndex = x + y * width;
+            const uint8_t y_val = pYUV->data[yIndex];
+            pYUV->data[yIndex] = (uint8_t) histogram_cdf[y_val];
+        }
+    }
+
+    // Step 4: Convert from YUV to RGB
+    convertYUVImageToRGBA(pYUV, pRGB);
+}
+
+#define clamp(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+
+static inline void convertYUVToRGBA(uint8_t y, uint8_t u, uint8_t v, uint8_t *buf) {
+    const int rTmp = y + (int)(1.370705f * (v - 128));
+    const int gTmp = y - (int)(0.698001f * (v - 128)) - (int)(0.337633f * (u - 128));
+    const int bTmp = y + (int)(1.732446f * (u - 128));
+
+    buf[0] = (uint8_t) clamp(rTmp, 0, 255);
+    buf[1] = (uint8_t) clamp(gTmp, 0, 255);
+    buf[2] = (uint8_t) clamp(bTmp, 0, 255);
+    buf[3] = 255; // Alpha channel
+}
+
+#ifdef __cplusplus
+}
+#endif
--- app/src/main/java/com/lauszus/facerecognitionapp/FaceRecognitionAppActivity.java
+++ app/src/main/java/com/lauszus/facerecognitionapp/FaceRecognitionAppActivity.java
@@ -1,29 +1,198 @@
 package com.lauszus.facerecognitionapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class FaceRecognitionAppActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_face_recognition_app);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
+public class FaceRecognitionAppActivity extends AppCompatActivity implements CvCameraViewListener2 {
+    private static final String TAG = FaceRecognitionAppActivity.class.getSimpleName();
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native void YUV2RGB(long matAddrYUV, long matAddrRgba);
+
+    public native void HistEQ(long matAddrYUV, long matAddrRgba);
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private Mat mRgba;
+    private Mat mGray;
+
+    private static final int PERMISSIONS_REQUEST_CODE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_face_recognition_app);
+        //setSupportActionBar((Toolbar) findViewById(R.id.toolbar)); // Sets the Toolbar to act as the ActionBar for this Activity window
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_calibration_java_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    System.loadLibrary("native-lib"); // Load native library after(!) OpenCV initialization
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.enableFpsMeter();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CODE:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    loadOpenCV();
+                } else {
+                    Toast.makeText(this, "Permission required!", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Request permission if needed
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSIONS_REQUEST_CODE);
+        else
+            loadOpenCV();
+    }
+
+    private void loadOpenCV() {
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+    }
+
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+        /*
+        if (mAbsoluteFaceSize == 0) {
+            int height = mGray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+            mNativeDetector.setMinFaceSize(mAbsoluteFaceSize);
+        }
+
+        MatOfRect faces = new MatOfRect();
+
+        if (mDetectorType == JAVA_DETECTOR) {
+            if (mJavaDetector != null)
+                mJavaDetector.detectMultiScale(mGray, faces, 1.1, 2, 2, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+        }
+        else if (mDetectorType == NATIVE_DETECTOR) {
+            if (mNativeDetector != null)
+                mNativeDetector.detect(mGray, faces);
+        }
+        else {
+            Log.e(TAG, "Detection method is not selected!");
+        }
+
+        Rect[] facesArray = faces.toArray();
+        for (int i = 0; i < facesArray.length; i++)
+            Imgproc.rectangle(mRgba, facesArray[i].tl(), facesArray[i].br(), FACE_RECT_COLOR, 3);
+        */
+        Core.flip(mGray, mGray, 1); // Flip image to get mirror effect
+        return mGray;
+    }
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(TAG, "called onCreateOptionsMenu");
+        mItemFace50 = menu.add("Face size 50%");
+        mItemFace40 = menu.add("Face size 40%");
+        mItemFace30 = menu.add("Face size 30%");
+        mItemFace20 = menu.add("Face size 20%");
+        mItemType   = menu.add(mDetectorName[mDetectorType]);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
+        if (item == mItemFace50)
+            setMinFaceSize(0.5f);
+        else if (item == mItemFace40)
+            setMinFaceSize(0.4f);
+        else if (item == mItemFace30)
+            setMinFaceSize(0.3f);
+        else if (item == mItemFace20)
+            setMinFaceSize(0.2f);
+        else if (item == mItemType) {
+            int tmpDetectorType = (mDetectorType + 1) % mDetectorName.length;
+            item.setTitle(mDetectorName[tmpDetectorType]);
+            setDetectorType(tmpDetectorType);
+        }
+        return true;
+    }
+    */
 }
