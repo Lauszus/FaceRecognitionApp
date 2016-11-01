499e566475bd91b60c4243a931080b25ceb28f36
Kristian Sloth Lauszus <lauszus@gmail.com>
2016-11-01 15:45:31 -0500

    Forgot to add copyright header

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,23 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Kristian Sloth Lauszus. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * Kristian Sloth Lauszus
+ * Web      :  http://www.lauszus.com
+ * e-mail   :  lauszus@gmail.com
+ ******************************************************************************/
+
 #include <jni.h>
-#include <opencv2/core/core.hpp>
+#include <opencv2/core.hpp>
 
 using namespace std;
 using namespace cv;
--- app/src/main/java/com/lauszus/facerecognitionapp/FaceRecognitionAppActivity.java
+++ app/src/main/java/com/lauszus/facerecognitionapp/FaceRecognitionAppActivity.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Kristian Sloth Lauszus. All rights reserved.
+ *
+ * This software may be distributed and modified under the terms of the GNU
+ * General Public License version 2 (GPL2) as published by the Free Software
+ * Foundation and appearing in the file GPL2.TXT included in the packaging of
+ * this file. Please note that GPL2 Section 2[b] requires that all works based
+ * on this software must also be made publicly available under the terms of
+ * the GPL2 ("Copyleft").
+ *
+ * Contact information
+ * -------------------
+ *
+ * Kristian Sloth Lauszus
+ * Web      :  http://www.lauszus.com
+ * e-mail   :  lauszus@gmail.com
+ ******************************************************************************/
+
 package com.lauszus.facerecognitionapp;
 
 import android.Manifest;
