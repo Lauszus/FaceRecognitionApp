a63b3442488b6dad9f2d5a35b1cd3764a54920e6
Kristian Sloth Lauszus <lauszus@gmail.com>
2018-10-01 12:11:49 +0200

    Fixed support for Android NDK r18, as gnustl has been removed
    See: https://developer.android.com/ndk/downloads/revision_history
    
    Fixes #22

--- app/src/main/cpp/Application.mk
+++ app/src/main/cpp/Application.mk
@@ -1,6 +1,6 @@
 APP_PLATFORM := android-27
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-APP_STL := gnustl_static
+APP_STL := c++_static
 APP_CPPFLAGS := -std=gnu++11 -frtti -fexceptions
 
 ifeq ($(NDK_DEBUG),0)
